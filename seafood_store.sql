07b82417361bcee471c74874ca4044e402781cbf
tdcoy510 <tdcoy510@gmail.com>
2022-04-06 21:11:41 -0400

    changed user field names

--- seafood_store.sql
+++ seafood_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2022 at 12:35 AM
+-- Generation Time: Apr 07, 2022 at 02:50 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -28,10 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `item` (
+  `item_id` int(5) NOT NULL,
   `item_name` varchar(255) NOT NULL,
-  `item_pice` decimal(10,2) DEFAULT NULL,
-  `item_descripion` varchar(255) DEFAULT NULL
+  `item_price` decimal(10,2) DEFAULT NULL,
+  `item_description` varchar(255) DEFAULT NULL,
+  `url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `item_name`, `item_price`, `item_description`, `url`) VALUES
+(0, 'Octopus', '12.00', 'this is octopus', ''),
+(1, 'Crab Legs', '6.00', 'this is crab legs', ''),
+(2, 'Shrimp', '15.00', 'this is shrimp', ''),
+(3, 'Mussels', '8.00', 'this is mussels', '');
 
 -- --------------------------------------------------------
 
@@ -40,21 +52,30 @@ CREATE TABLE `item` (
 --
 
 CREATE TABLE `order` (
-  `id` int(5) NOT NULL,
+  `order_number` int(5) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `date_purchase` date NOT NULL
+  `date_purchase` date NOT NULL,
+  `user_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`order_number`, `email`, `date_purchase`, `user_id`) VALUES
+(1, 'luu@gmail.com', '2022-04-06', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order table`
+-- Table structure for table `order_table`
 --
 
-CREATE TABLE `order table` (
-  `id` int(5) NOT NULL,
+CREATE TABLE `order_table` (
+  `order_id` int(5) NOT NULL,
   `item_name` varchar(255) DEFAULT NULL,
-  `item_quantity` int(3) DEFAULT NULL
+  `item_quantity` int(3) DEFAULT NULL,
+  `order_number` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -64,12 +85,22 @@ CREATE TABLE `order table` (
 --
 
 CREATE TABLE `user` (
-  `email` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `phone_number` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `user_id` int(5) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_first_name` varchar(255) NOT NULL,
+  `user_last_name` varchar(255) NOT NULL,
+  `user_phone_number` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `email`, `first_name`, `last_name`, `phone_number`, `password`, `user_type`) VALUES
+(1, 'luu@gmail.com', 'luong', 'luu', '123456789', '1234', 'admin'),
+(3, 'lee@gmail.com', 'Kevin', 'Lee', '234516789', '23451', 'customer');
 
 --
 -- Indexes for dumped tables
@@ -79,25 +110,36 @@ CREATE TABLE `user` (
 -- Indexes for table `item`
 --
 ALTER TABLE `item`
-  ADD PRIMARY KEY (`item_name`);
+  ADD PRIMARY KEY (`item_id`),
+  ADD UNIQUE KEY `item_name` (`item_name`);
 
 --
 -- Indexes for table `order`
 --
 ALTER TABLE `order`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`order_number`),
+  ADD UNIQUE KEY `order_id` (`order_number`),
+  ADD UNIQUE KEY `order_number` (`order_number`),
+  ADD KEY `fk_userorder` (`user_id`);
 
 --
--- Indexes for table `order table`
+-- Indexes for table `order_table`
 --
-ALTER TABLE `order table`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `order_table`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_order` (`order_number`),
+  ADD KEY `fk_itemname` (`item_name`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `id` (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone_number` (`phone_number`),
+  ADD UNIQUE KEY `password_2` (`password`),
+  ADD KEY `password_3` (`password`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -107,7 +149,30 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_number` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_userorder` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order_table`
+--
+ALTER TABLE `order_table`
+  ADD CONSTRAINT `fk_itemname` FOREIGN KEY (`item_name`) REFERENCES `item` (`item_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_order` FOREIGN KEY (`order_number`) REFERENCES `order` (`order_number`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
