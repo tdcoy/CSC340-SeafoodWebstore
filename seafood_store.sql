77c7d8c0ee1555c5471033bb73c5815276fb49e4
Luong Luu <luongluu2001@gmail.com>
2022-04-10 16:20:10 -0400

    Update seafood_store.sql

--- seafood_store.sql
+++ seafood_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2022 at 10:18 PM
+-- Generation Time: Apr 10, 2022 at 10:19 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
