f2f353e9ee2df4066ca0ac73fc6f091adff03830
terrence <t_coy@uncg.edu>
2022-04-13 15:46:34 -0400

    Fixed Orders

--- includes/orders.inc.php
+++ includes/orders.inc.php
@@ -1,13 +1,19 @@
 <?php
-if (isset($_POST['order-submit'])) {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    require_once '../db_connect.php';
-
-    
-
-    } else {
+ if (isset($_GET['id'])){
+        require_once '../db_connect.php';
+        // Store the value from get to a 
+        // local variable "course_id"
+        $course_id=$_GET['id'];
+  
+        // SQL query that sets the status
+        // to 1 to indicate activation.
+        $sqlUpdateStatus = "UPDATE orders SET order_status = '1' WHERE order_number = $course_id";
+  
+        // Execute the query
+        mysqli_query($conn,$sqlUpdateStatus);
+    }
+  
+    // Go back to course-page.php
     header("location: ../html/ordersvendor.php");
-    exit();
-}
+
+?>
